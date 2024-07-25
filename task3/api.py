6a7ad886428c7320a2be2527b47e702e5212d458
Sophie Renshaw <sophierenshaw@sophies-air.lan>
2024-07-25 13:47:52 +0100

    adding comments for task3

--- task3/api.py
+++ task3/api.py
@@ -9,11 +9,13 @@ from sqlalchemy.orm import Session
 
 from pydantic import BaseModel, Field
 
+#creating a mocked database
 mocked_db = {
     "query1": {"query": "How do I reset my device to factory settings?", "answer": "To reset your device to factory settings, go to 'Settings' > 'System' > 'Reset' > 'Factory data reset'. Confirm the reset by following the on-screen instructions. Please note that this will erase all data on the device."},
     "query2": {"query": "What should I do if my device won't turn on?", "answer": "If your device won't turn on, try the following steps: \n   - Ensure the device is charged by connecting it to the charger for at least 30 minutes. \n   - Press and hold the power button for 10-15 seconds to force a restart. \n   - If the device still does not turn on, try using a different charger or cable. \n   - If none of these steps work, please contact customer support for further assistance."},
 }
 
+#creating a sqlite database
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
@@ -47,28 +49,37 @@ class QueryModel(Base):
 
 app = FastAPI()
 
+#crud operation to retrieve the query information
 def get_query(db: Session, query: str):
     return db.query(QueryModel).filter(QueryModel.query == query).first()
 
+#GET request to retrieve the query information
 @app.get("/query")
 def query(query: str):
     if not query:
+        #throws a 400 error if the query parameter is not provided
         raise HTTPException(status_code=400, detail="Query parameter is required")
     return {"message": f"Query received: {query}"}
 
+#GET request to retrieve the query information from the mocked database
 @app.get("/query/{query}", response_model=Query)
 def get_mocked_query(query: str):
     if not query:
+        #throws a 400 error if the query parameter is not provided
         raise HTTPException(status_code=400, detail="Query parameter is required")
     if query not in mocked_db:
+        #throws a 404 error if the query is not found in the database
         raise HTTPException(status_code=404, detail="Item not found")
     return mocked_db[query]
 
+#GET request to retrieve the query information from the sqlite database
 @app.get("/query/sql/{query}", response_model=Query)
 def get_sql_query(query: str, db: Session = Depends(get_db)):
     if not query:
+        #throws a 400 error if the query parameter is not provided
         raise HTTPException(status_code=400, detail="Query parameter is required")
     db_query = get_query(db, query=query)
     if db_query is None:
+        #throws a 404 error if the query is not found in the database
         raise HTTPException(status_code=404, detail="No result")
     return db_query
--- task3/test_api.py
+++ task3/test_api.py
@@ -6,6 +6,7 @@ from sqlalchemy.pool import StaticPool
 
 from .api import app, get_db, Base
 
+#SQLite database for testing
 SQLALCHEMY_DATABASE_URL = "sqlite://"
 
 engine = create_engine(
@@ -14,9 +15,7 @@ engine = create_engine(
     poolclass=StaticPool,
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base.metadata.create_all(bind=engine)
-
 
 def override_get_db():
     try:
@@ -29,31 +28,33 @@ app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
+#Test for valid query parameter
 def test_query_with_valid_input():
     response = client.get("/query?query=Hello")
     assert response.status_code == 200
     assert response.json() == {"message": f"Query received: Hello"}
 
+#Test for valid query parameter with mocked database
 def test_query_with_valid_input_mocked_db():    
     response = client.get("/query/How do I reset my device to factory settings?")
     data = response.json()
-    print(data)
     assert response.status_code == 200
     assert data["answer"] == "To reset your device to factory settings, go to 'Settings' > 'System' > 'Reset' > 'Factory data reset'. Confirm the reset by following the on-screen instructions. Please note that this will erase all data on the device."
 
+#Test for valid query parameter with sqlite database
 def test_query_with_valid_input_sql_db():    
     response = client.get("/query/db/How do I reset my device to factory settings?")
     data = response.json()
-    print(data)
     assert response.status_code == 200
     assert data["answer"] == "To reset your device to factory settings, go to 'Settings' > 'System' > 'Reset' > 'Factory data reset'. Confirm the reset by following the on-screen instructions. Please note that this will erase all data on the device."
 
-
+#Test for no query parameter
 def test_query_with_empty_input():
     response = client.get("/query?query=")
     assert response.status_code == 400
     assert response.json() == {"detail": "Query parameter is required"}
 
+#Test for query parameter not provided
 def test_query_without_input():
     response = client.get("/query")
     assert response.status_code == 422
